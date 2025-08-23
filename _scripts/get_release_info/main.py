ecc13bf08bf446c228f888cbd7bc4888f0d803fe
insolor <2442833+insolor@users.noreply.github.com>
2025-08-23 23:44:43 +0300

    Draft of script code

--- _scripts/get_release_info/main.py
+++ _scripts/get_release_info/main.py
@@ -1,5 +1,35 @@
+from pathlib import Path
+
+import httpx
+
+
+current_dir = Path(__file__).parent
+query_get_last_release_assets = (current_dir / "getLatestReleaseAssets.graphql").read_text()
+url = "http://api.github.com/graphql"
+
+repo_root_dir = current_dir.parent.parent
+output_file = repo_root_dir / "_data/release_assets.json"
+
+
 def main():
-    print("Hello from get-release-info!")
+    response = httpx.post(
+        url,
+        json={
+            "query": query_get_last_release_assets,
+            "variables": {
+                "owner": "dfint",
+                "repo": "installer",
+                "first": 2,
+            }
+        },
+        follow_redirects=True,
+    )
+    response.raise_for_status()
+
+    response_json = response.json()
+    assets = response_json["data"]["repository"]["latestRelease"]["releaseAssets"]["nodes"]
+    for asset in assets:
+        print(f"[{asset['name']}]({asset['downloadUrl']})")
 
 
 if __name__ == "__main__":
