2c1100dc29380363e024e3c732d1f399a03a2e27
insolor <2442833+insolor@users.noreply.github.com>
2025-08-26 16:19:39 +0300

    Add token, fixes

--- _scripts/get_release_info/main.py
+++ _scripts/get_release_info/main.py
@@ -1,14 +1,20 @@
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 import httpx
+
+load_dotenv()
 
 current_dir = Path(__file__).parent
 query_get_last_release_assets = (current_dir / "getLatestReleaseAssets.graphql").read_text()
-url = "http://api.github.com/graphql"
+url = "https://api.github.com/graphql"
 
 repo_root_dir = current_dir.parent.parent
 assert (repo_root_dir / ".git").exists()
 output_file = repo_root_dir / "_data/release_assets.json"
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 
 def main():
@@ -18,18 +24,20 @@ def main():
             "query": query_get_last_release_assets,
             "variables": {
                 "owner": "dfint",
-                "repo": "installer",
+                "name": "installer",
                 "first": 2,
             }
         },
         follow_redirects=True,
+        headers={"Authorization": f"Bearer {GITHUB_TOKEN}"},
+        timeout=httpx.Timeout(60)
     )
     response.raise_for_status()
 
     response_json = response.json()
     assets = response_json["data"]["repository"]["latestRelease"]["releaseAssets"]["nodes"]
     for asset in assets:
-        print(f"[{asset['name']}]({asset['downloadUrl']})")
+        print(f"{asset['name']}: {asset['downloadUrl']}")
 
 
 if __name__ == "__main__":
